e38bb3e8dedf4300818f573af9446a326d2ec41f
Gardiano <joaopaulong@hotmail.com>
2020-12-06 01:14:18 -0300

    css resp changes

--- src/Components/login/login.js
+++ src/Components/login/login.js
@@ -1,23 +1,26 @@
-import React, {Component} from 'react';
-import './login.css';
+import React, { Component } from "react";
+import "./login.css";
 
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default class Login extends Component {
-    render() {
-        return(
-        <div>
-            <div className="login-box">
-                <div className="login-logo">                 
-                   <i class="fas fa-video"> VídeoBox                    
-                    <button> 
-                        <Link to="/Home">
-                            <i class="fas fa-long-arrow-alt-right"></i>
-                        </Link> </button>
-                    </i>                   
-                </div> 
-            </div>                              
+  render() {
+    return (
+      <div class="container">     
+        <div class="box"> 
+        <p> VIDEOBOX </p> 
+                 <button>
+                 <Link to="/home">
+                 <i id="icon-arrow" class="fas fa-long-arrow-alt-right"> </i>
+                 </Link>
+                 </button>
+          </div>
         </div>
-        );
-    }
+    );
+  }
 }
+
+
+  /* <i id="icon-video" class="fas fa-video"></i>
+                <i class="fas fa-long-arrow-alt-right"></i>   */
+
